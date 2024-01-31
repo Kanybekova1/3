699d3982338f8e9d82910ce11ec43e5558d5d390
Kanybekova1 <150647176+Kanybekova1@users.noreply.github.com>
2024-01-31 20:20:47 +0600

    Add files via upload

---
+++ myfamily.py
@@ -0,0 +1,9 @@
+myfamily = ("mother", "father", "sister", "brother", "sister") 
+print(type(myfamily)) 
+print(myfamily[2])
+
+a=("me",)
+myfamily+=a
+print(myfamily)
+myfamily.pop("brother")
+print(myfamily)
