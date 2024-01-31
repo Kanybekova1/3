671ad3055b5f6fb94d97393095acc5c1a7338c20
Kanybekova1 <150647176+Kanybekova1@users.noreply.github.com>
2024-01-31 20:21:37 +0600

    Add files via upload

---
+++ user.py
@@ -0,0 +1,13 @@
+dict={}
+
+for i in range(1):
+    name=input("What is the user's name?: ")
+    age=input("What is the user's age?: ")
+    country=input("What is the user's country of birth?: ")
+    known_for=input("What is the user known for?: ")
+
+    dict["Name"]= name
+    dict["Age"]= age
+    dict["Country"]=country
+    dict["Known for"]=known_for
+
